7cf7fbbc8fe86daa7a45ec9221191596be687a6b
pratap86 <narayanpratap86@gmail.com>
2021-06-29 10:17:01 +0530

    initial controller

---
+++ src/main/java/com/pratap/estore/product/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.pratap.estore.product.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public String getProduct(){
+        return "Get Mapping";
+    }
+}
